1611d5a778de776a65d8f31ab42d42852cc611e1
ambiesoft <ambiesoft.trueff@gmail.com>
2019-02-04 18:32:23 +0900

    edit

--- vs/stepfiltertest/stepfiltertest/main.cpp
+++ vs/stepfiltertest/stepfiltertest/main.cpp
@@ -12,6 +12,9 @@ public:
 	C() {
 		cout << "C" << endl;
 	}
+	C(int i) {
+		cout << "C" << i << endl;
+	}
 	~C() {
 		cout << "~C" << endl;
 	}
@@ -21,7 +24,12 @@ int main()
 	string s;
 	s = "aaa";
 
+	vector<int> vi;
+	vi.push_back(1);
+	vi.emplace_back(2);
+
 	unique_ptr<C> uc = make_unique<C>();
+	unique_ptr<C> uci = make_unique<C>(33);
 	shared_ptr<C> sc = make_shared<C>();
 	return 0;
 }
