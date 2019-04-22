c88d9eb44718b0d06f524814c54bd0e8a6c6d436
ambiesoft <ambiesoft.trueff@gmail.com>
2019-04-22 11:23:26 +0900

    2019

--- gbin/dev2017.bat
+++ gbin/dev2017.bat
@@ -1,1 +1,1 @@
-start "" "Z:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" %*
+start "" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" %*
