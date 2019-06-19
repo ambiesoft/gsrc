9c49fb0a59bb86fdc025a6db1939a8166dba0b6e
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-19 16:14:20 +0900

    many

--- mysrc/mymain.cc
+++ mysrc/mymain.cc
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <base/cpu.h>
 
 int main()
 {
 	std::cout << "MYMAIN!" << std::endl;
+	
+	base::CPU cpu;
+	std::cout << "has_sse:" << cpu.has_sse() << std::endl;
+	
 	return 0;
 }
