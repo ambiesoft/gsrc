525c5522bf279ac770b855502dce209e7eaaddfc
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-20 17:08:13 +0900

    misc

---
+++ gbin/cnote.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\Linkout\cnote\cnote.exe %*
--- gbin/dev2019.bat
+++ gbin/dev2019.bat
@@ -1,4 +1,6 @@
+@echo off
 REM set INCLUDE=%~dp0..\src
 REM set PATH=%INCLUDE%;%PATH%
+@echo on
 
 start "" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" %*
