83ef2647c6c89cdf1d506cf3bc359aa4ff5536e0
ambiesoft <ambiesoft.trueff@gmail.com>
2019-06-23 13:46:33 +0900

    add gnrefsr

---
+++ gbin/gnrefsr.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import subprocess
+
+import real2gn
+
+''' call gnref with realpath input'''
+
+def isOption(arg):
+    return arg[0]=='-'
+
+def main():
+    args = ['cmd', '/c', os.path.join(os.path.dirname(__file__), 'gnrefs')]
+    
+    for arg in sys.argv[1:]:
+        if isOption(arg):
+            args.append(arg)
+        else:
+            args.append(real2gn.real2gn(arg))
+
+    subprocess.call(args)
+
+if __name__ == "__main__":
+    main()
