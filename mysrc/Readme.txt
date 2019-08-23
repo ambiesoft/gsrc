63a57ac8f649ad33190d0ebbf588f0f84de9e3dd
ambiesoft <ambiesoft.trueff@gmail.com>
2019-08-24 00:53:55 +0900

    add

---
+++ mysrc/Readme.txt
@@ -0,0 +1,5 @@
+1, run mingw
+2, cd /c/gsrc
+3, cat mysrc/addthistosrcBuild.ng.txt >> chromium/src/BUILD.gn
+4, open command
+5, C:\gsrc>mklink /D /J chromium\src\mysrc mysrc
--- mysrc/mymain.cc
+++ mysrc/mymain.cc
@@ -1,12 +1,18 @@
-#include <iostream>
 #include <base/cpu.h>
+#include <iostream>
 
-int main()
-{
-	std::cout << "MYMAIN!" << std::endl;
-	
-	base::CPU cpu;
-	std::cout << "has_sse:" << cpu.has_sse() << std::endl;
-	
-	return 0;
+int main2() {
+  std::cout << "MYMAIN!" << std::endl;
+
+  base::CPU cpu;
+  std::cout << "has_sse:" << cpu.has_sse() << std::endl;
+
+  std::cout << "has_avx:" << cpu.has_avx() << std::endl;
+  return 0;
+}
+
+int main() {
+  main2();
+
+  return 0;
 }
