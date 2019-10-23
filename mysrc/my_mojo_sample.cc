9cdd12ef1089f6639d40b2b2f99915fb08c15482
ambiesoft <ambiesoft.trueff@gmail.com>
2019-10-23 15:01:59 +0900

    many

--- mysrc/my_mojo_sample.cc
+++ mysrc/my_mojo_sample.cc
@@ -1,8 +1,11 @@
 #include "base/process/process_handle.h"
 #include "base/threading/thread.h"
+#include "base/memory/platform_shared_memory_region.h"
+
 #include "mojo/core/embedder/embedder.h"
-#include "mojo/core/embedder/outgoing_broker_client_invitation.h"
-#include "mojo/core/embedder/platform_channel_pair.h"
+// #include "mojo/core/embedder/outgoing_broker_client_invitation.h"
+// #include "mojo/core/embedder/platform_channel_pair.h"
+#include "mojo/core/platform_handle_utils.h"
 #include "mojo/core/embedder/scoped_ipc_support.h"
 
 // You write this. It launches a new process, passing the pipe handle
@@ -10,8 +13,8 @@
 // you may inhert the file descriptor/HANDLE at launch and pass a commandline
 // argument to indicate its numeric value). Returns the handle of the new
 // process.
-base::ProcessHandle LaunchCoolChildProcess(
-    mojo::core::ScopedPlatformHandle channel);
+//base::ProcessHandle LaunchCoolChildProcess(
+//    mojo::core::ScopedPlatformHandle channel);
 
 int main(int argc, char** argv) {
   mojo::core::Init();
@@ -26,20 +29,26 @@ int main(int argc, char** argv) {
 
   // This is essentially always an OS pipe (domain socket pair, Windows named
   // pipe, etc.)
-  mojo::core::PlatformChannelPair channel;
+  //mojo::core::PlatformChannelPair channel;
+
+  //// This is a scoper which encapsulates the intent to connect to another
+  //// process. It exists because process connection is inherently asynchronous,
+  //// things may go wrong, and the lifetime of any associated resources is bound
+  //// by the lifetime of this object regardless of success or failure.
+  //mojo::core::OutgoingBrokerClientInvitation invitation;
 
   // This is a scoper which encapsulates the intent to connect to another
-  // process. It exists because process connection is inherently asynchronous,
-  // things may go wrong, and the lifetime of any associated resources is bound
-  // by the lifetime of this object regardless of success or failure.
-  mojo::core::OutgoingBrokerClientInvitation invitation;
+// process. It exists because process connection is inherently asynchronous,
+// things may go wrong, and the lifetime of any associated resources is bound
+// by the lifetime of this object regardless of success or failure.
+  //mojo::edk::PendingProcessConnection child;
 
-  base::ProcessHandle child_handle =
-      LaunchCoolChildProcess(channel.PassClientHandle());
+  //base::ProcessHandle child_handle =
+  //    LaunchCoolChildProcess(channel.PassClientHandle());
 
-  // At this point it's safe for |child| to go out of scope and nothing will
-  // break.
-  child.Connect(child_handle, channel.PassServerHandle());
+  //// At this point it's safe for |child| to go out of scope and nothing will
+  //// break.
+  //child.Connect(child_handle, channel.PassServerHandle());
 
   return 0;
 }
